04ead5ba54da9a12b96a9f62af5ea6e7b1851642
Wesling_Hernandez <wesling.hernandez@golabstech.com>
2022-06-08 17:20:24 -0600

    new changes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -13,7 +13,18 @@ const GifExpertApp = ({defaultCategories = []}) => {
     
     return(
         <>
-            <h2>GifExpertApp</h2>
+        <div className="wrapper">
+            <h1 className="right">Costa Rica, 8 de Junio de 2022</h1>
+            <p>Asunto: La dedicación de una canción</p>
+            <br/>
+            <p>Srta. Sara</p>
+            <p>Directora general de GOAT</p>
+            <p>Se le ha realizado este sitio web por motivos de que escuche la siguiente canción</p>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/sruVq3ZrmPc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>Saludos cordiales</p>
+            <p>Wesling Hernández Medina</p>
+        </div>
+            {/* <h2>GifExpertApp</h2>
             <AddCategory setCategories={setCategories}/>
             <hr/>
         
@@ -27,7 +38,7 @@ const GifExpertApp = ({defaultCategories = []}) => {
                         />
                     )
                 }
-            </ol>
+            </ol> */}
         </>
     );
 }
